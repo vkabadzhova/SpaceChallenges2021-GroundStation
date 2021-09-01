77452ed1ca1c92172dacd862e0f0dedbec4579c1
antonAtananasov <anton_atananasov@abv.bg>
2021-09-01 12:11:27 +0300

    Changed doppler effect calculation method to use vector operations

--- calcdoppler.py
+++ calcdoppler.py
@@ -9,11 +9,14 @@ lon, lat, ao = gela #ao is observer altitude above sea level
 sat = Orbital("QMR-KWT")
 satlon,satlat, asat = sat.get_lonlatalt(datetime.utcnow()) #altitude of satellite
 el = sat.get_observer_look(datetime.utcnow(),lon, lat, ao) # elevation angle towards sat
-
+'''
 def getSatVelocity(sat, utcTime): #sat = Orbital('QMR-KWT') or sth
 	Vvector = sat.get_position(utcTime,0)[1] #velocity vector
 	Vs = sqrt(np.sum(np.square(np.multiply(Vvector,1e3))))
 	return Vs
+'''
+def getVectorMagnitude(v):
+	return np.sqrt(np.sum(np.square(v)))
 
 def calcReAtLat(lat): # the earth is a potato  
 	r1 = 6378.14e3 #earth equatorial radius
@@ -23,17 +26,45 @@ def calcReAtLat(lat): # the earth is a potato
 	Re=sqrt(((r1**2*cos(lat))**2+(r2**2*sin(lat))**2)/((r1*cos(lat))**2+(r2*sin(lat))**2)) # (formula from https://rechneronline.de/earth-radius/ )
 	return Re/1e3
 
+def getAngleBetweenVectors(v1,v2):
+	unit_v1 = v1 / np.linalg.norm(v1)
+	unit_v2 = v2 / np.linalg.norm(v2)
+	dot_product = np.dot(unit_v1, unit_v2)
+	angle = degrees(np.arccos(dot_product))
+	return angle
+
+def calcTriangleToObserver(sat,lon,lat,alt, utc):
+	satPosVector = sat.get_position(utc, 0)[0]
+	satDistance = getVectorMagnitude(satPosVector) # to center of earth
+	r = calcReAtLat(lat)+alt/1000
+	x = r*cos(lat)*cos(lon)
+	y = r*cos(lat)*sin(lon)
+	z = r*sin(lat)
+	observerPosVector = (x,y,z)
+	observerDistance = getVectorMagnitude(observerPosVector) # to center of earth
+	distanceVector = np.subtract(satPosVector, observerPosVector)
+	distanceMagnitude = getVectorMagnitude(distanceVector) # from sat to observer
+	return distanceMagnitude, observerDistance, satDistance, distanceVector
+
+def getSatRelativeVelocity(sat, utc,lon,lat,alt):
+	distanceMagnitude, observerDistance, satDistance, distanceVector = calcTriangleToObserver(sat,lon,lat,alt, utc)
+	satPosVector, satVelocityVector = np.multiply(sat.get_position(utc, 0),1000)
+	angle = getAngleBetweenVectors(satVelocityVector,distanceVector)
+	velocity = getVectorMagnitude(satVelocityVector)
+	relativeVelocity = velocity*cos(radians(angle))
+	return relativeVelocity
+'''
 def calcAngleToObserver(Re,ao,asat,el): #potato radius, observer altitude, satellite altitude, elevation from horizon
 	cosElPN = cos(radians(el+90)) # cos(el+90deg)
 	x = (Re+ao)*cosElPN+sqrt((Re+ao)**2*(cosElPN**2)+(Re+asat)**2) #distance between sat and observer
 	cosGamma = ((Re+asat)**2+(Re+ao)**2-x**2)/(2*(Re+asat)*(Re+ao)) #calculate cosine of gamma (using cosine theorem) where gamma is the angle between the observer and the satellite from the center of the earth
 	gamma = acos(cosGamma) # get the angle (in radians)
 	return gamma 
-
+'''
 def calcDopplerShift(f,Vsx,Vo=0): #difference in frequencies (in KHz) for stationary observer
 	#frequency in MHz, 1d velocity of satellite, 1d velocity of observer 
 	c=299792458#speed of light in meters
 	df=f*((c+Vo)/(c+Vsx)-1)
 	return df*1e3
 
-print(calcDopplerShift(436.5,getSatVelocity(sat,datetime.utcnow())),'KHz')
+print(calcDopplerShift(436.5,getSatRelativeVelocity(sat, datetime.utcnow(),lon,lat,ao)),'KHz')
