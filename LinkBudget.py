af18b5b3817383a3f469c7a63948c072e4b39898
Silverlined <dimitriy.georgiev@gmail.com>
2021-09-01 11:23:04 +0300

    Refactoring, add Link Budget

--- LinkBudget.py
+++ LinkBudget.py
@@ -1,4 +1,4 @@
-import numpy as np
+from math import log10, pi
 
 """ 
 QMR - KWT 
@@ -11,15 +11,33 @@ G_r -> Gain of ground station antenna
 T_s -> System temperature
 R -> Transmission rate
 La -> Atmospheric losses
+Lp -> Polarization losses
 Ls -> Free space losses
 Kb -> Botlzman constant
 C -> Speed of light
 
 """
+c = 299792458 # [m/s]
+
+G_t = 0
+P_t = -3.5  # [dBW]
+h_max = 2.5e6 # [m]
+f   = 436.5e6 # [MHz]
+G_r = 16.6    # [dBi]
+T_s = 320   # [K]
+R   = 9600  # [bps]
+La = 1  # [dB]
+Lp = 3  # [dB]
+Ls = 20 * log10(4 * pi * h_max * f / c) #[dB]
+Kb = 228.5 # [dB]
+
+def getEbNo():
+    Gr_Ts = G_r - 10 * log10(T_s)
+    return G_t + P_t + Gr_Ts - La - Ls - Lp + Kb - 10 * log10(R)
 
 def main():
-
+    Eb_No = getEbNo()
     print("Energy per bit per noise ratio (Eb/No): ", Eb_No)
 
-if __name__ == "__main__ ":
+if __name__ == "__main__":
     main()
--- calcdoppler.py
+++ calcdoppler.py
@@ -5,35 +5,50 @@ import pyorbital
 from pyorbital.orbital import Orbital
 
 gela=(24.5730, 41.6500015, 1463)    # Coordinates of Gela, Bulgaria 
-lon, lat, ao = gela #ao is observer altitude above sea level
+lon, lat, altitude_gs = gela # altitude_gs is observer altitude above sea level
 sat = Orbital("QMR-KWT")
-satlon,satlat, asat = sat.get_lonlatalt(datetime.utcnow()) #altitude of satellite
-el = sat.get_observer_look(datetime.utcnow(),lon, lat, ao) # elevation angle towards sat
+satlon, satlat, altitude_sat = sat.get_lonlatalt(datetime.utcnow()) #altitude of satellite
+az, el = sat.get_observer_look(datetime.utcnow(),lon, lat, altitude_gs) # elevation angle towards sat
+frequency = 436.5
 
-def getSatVelocity(sat, utcTime): #sat = Orbital('QMR-KWT') or sth
+def getSatelliteVelocity(sat, utcTime): #sat = Orbital('QMR-KWT') or sth
 	Vvector = sat.get_position(utcTime,0)[1] #velocity vector
 	Vs = sqrt(np.sum(np.square(np.multiply(Vvector,1e3))))
 	return Vs
 
-def calcReAtLat(lat): # the earth is a potato  
+def getRadiusFromLatitude(lat): # the earth is a potato  
 	r1 = 6378.14e3 #earth equatorial radius
 	r2 = 6356.75e3 #earth polar radius
-	#(values from https://calgary.rasc.ca/latlong.htm)
+	# (values from https://calgary.rasc.ca/latlong.htm)
 	lat = radians(lat)
-	Re=sqrt(((r1**2*cos(lat))**2+(r2**2*sin(lat))**2)/((r1*cos(lat))**2+(r2*sin(lat))**2)) # (formula from https://rechneronline.de/earth-radius/ )
-	return Re/1e3
+	earth_radius = sqrt(((r1**2*cos(lat))**2+(r2**2*sin(lat))**2)/((r1*cos(lat))**2+(r2*sin(lat))**2)) # (formula from https://rechneronline.de/earth-radius/ )
+	return earth_radius / 1e3
 
-def calcAngleToObserver(Re,ao,asat,el): #potato radius, observer altitude, satellite altitude, elevation from horizon
+def getDistanceToObserver(earth_radius, altitude_gs, altitude_sat):
 	cosElPN = cos(radians(el+90)) # cos(el+90deg)
-	x = (Re+ao)*cosElPN+sqrt((Re+ao)**2*(cosElPN**2)+(Re+asat)**2) #distance between sat and observer
-	cosGamma = ((Re+asat)**2+(Re+ao)**2-x**2)/(2*(Re+asat)*(Re+ao)) #calculate cosine of gamma (using cosine theorem) where gamma is the angle between the observer and the satellite from the center of the earth
-	gamma = acos(cosGamma) # get the angle (in radians)
-	return gamma 
+	x = (earth_radius+altitude_gs)*cosElPN+sqrt((earth_radius+altitude_gs)**2*(cosElPN**2)+(earth_radius+altitude_sat)**2) #distance between sat and observer
+	return x
 
-def calcDopplerShift(f,Vsx,Vo=0): #difference in frequencies (in KHz) for stationary observer
-	#frequency in MHz, 1d velocity of satellite, 1d velocity of observer 
-	c=299792458#speed of light in meters
+def getAngleToObserver(earth_radius, altitude_gs, altitude_sat, el): #potato radius, observer altitude, satellite altitude, elevation from horizon
+	cosElPN = cos(radians(el+90)) # cos(el+90deg)
+	x = (earth_radius+altitude_gs)*cosElPN+sqrt((earth_radius+altitude_gs)**2*(cosElPN**2)+(earth_radius+altitude_sat)**2) #distance between sat and observer
+	cosGamma = ((earth_radius+altitude_sat)**2+(earth_radius+altitude_gs)**2-x**2)/(2*(earth_radius+altitude_sat)*(earth_radius+altitude_gs)) #calculate cosine of gamma (using cosine theorem) where gamma is the angle between the observer and the satellite from the center of the earth
+	gamma = acos(cosGamma) # get the angle (in radians)
+	return gamma
+
+def getDopplerShift(f,Vsx,Vo=0): #difference in frequencies (in KHz) for stationary observer
+	# frequency in MHz, 1d velocity of satellite, 1d velocity of observer 
+	c = 299792458 # speed of light in meters per sec
 	df=f*((c+Vo)/(c+Vsx)-1)
 	return df*1e3
 
-print(calcDopplerShift(436.5,getSatVelocity(sat,datetime.utcnow())),'KHz')
+
+def main():
+	earth_radius = getRadiusFromLatitude(lat)
+	gamma = getAngleToObserver(earth_radius, altitude_gs, altitude_sat, el)
+	Vs_x = cos(getAngleToObserver(earth_radius, altitude_gs, altitude_sat, el) + np.deg2rad(el)) * getSatelliteVelocity(sat,datetime.utcnow())
+	print(getDopplerShift(frequency, Vs_x),'KHz')
+
+if __name__ == "__main__":
+    main()
+
